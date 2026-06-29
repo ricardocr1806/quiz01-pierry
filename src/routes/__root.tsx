4974666650194aa02ae0221989a36fe8535cc46f
ricardocr1806 <ricardo.castro1806@hotmail.com>
2026-06-28 21:48:01 -0300

    fix: pixel ID atualizado para 1563894625335592

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -118,12 +118,12 @@ function RootShell({ children }: { children: ReactNode }) {
           t.src=v;s=b.getElementsByTagName(e)[0];
           s.parentNode.insertBefore(t,s)}(window, document,'script',
           'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '1574746134050851');
+          fbq('init', '1563894625335592');
           fbq('track', 'PageView');
         `}} />
         <noscript dangerouslySetInnerHTML={{ __html: `
           <img height="1" width="1" style="display:none"
-          src="https://www.facebook.com/tr?id=1574746134050851&ev=PageView&noscript=1"/>
+          src="https://www.facebook.com/tr?id=1563894625335592&ev=PageView&noscript=1"/>
         `}} />
       </head>
       <body>
